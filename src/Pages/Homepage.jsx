5d0581bfdfe90d0ae64f3f649d9c46e1dc9736b8
Abner Nieves <nieves.abner@gmail.com>
2024-06-03 18:52:46 -0400

    added C R with form got data from server

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,62 +1,66 @@
 import React from 'react'
-
+import AddMusic from '../components/AddMusic';
 
 function Homepage() {
   return (
-    <div>
-      <div id="carouselExampleIndicators" className="carousel slide">
-        <div className="carousel-indicators">
-          <button 
-            type="button" 
-            data-bs-target="#carouselExampleIndicators" 
-            data-bs-slide-to="0" 
-            className="active" 
-            aria-current="true" 
-            aria-label="Slide 1"
-          ></button>
-          <button 
-            type="button" 
-            data-bs-target="#carouselExampleIndicators" 
-            data-bs-slide-to="1" 
-            aria-label="Slide 2"
-          ></button>
-          <button 
-            type="button" 
-            data-bs-target="#carouselExampleIndicators" 
-            data-bs-slide-to="2" 
-            aria-label="Slide 3"
-          ></button>
-        </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src='src\assets\lib\terror-background.jpg' className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src="src\assets\lib\terror3.jpg" className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src="src\assets\lib\terror-background.jpg" className="d-block w-100" alt="..." />
-          </div>
-        </div>
-        <button 
-          className="carousel-control-prev" 
-          type="button" 
-          data-bs-target="#carouselExampleIndicators" 
-          data-bs-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button 
-          className="carousel-control-next" 
-          type="button" 
-          data-bs-target="#carouselExampleIndicators" 
-          data-bs-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
+    // <div>
+    //   <div id="carouselExampleIndicators" className="carousel slide">
+    //     <div className="carousel-indicators">
+    //       <button 
+    //         type="button" 
+    //         data-bs-target="#carouselExampleIndicators" 
+    //         data-bs-slide-to="0" 
+    //         className="active" 
+    //         aria-current="true" 
+    //         aria-label="Slide 1"
+    //       ></button>
+    //       <button 
+    //         type="button" 
+    //         data-bs-target="#carouselExampleIndicators" 
+    //         data-bs-slide-to="1" 
+    //         aria-label="Slide 2"
+    //       ></button>
+    //       <button 
+    //         type="button" 
+    //         data-bs-target="#carouselExampleIndicators" 
+    //         data-bs-slide-to="2" 
+    //         aria-label="Slide 3"
+    //       ></button>
+    //     </div>
+    //     <div className="carousel-inner">
+    //       <div className="carousel-item active">
+    //         <img src='src\assets\lib\terror-background.jpg' className="d-block w-100" alt="..." />
+    //       </div>
+    //       <div className="carousel-item">
+    //         <img src="src\assets\lib\terror3.jpg" className="d-block w-100" alt="..." />
+    //       </div>
+    //       <div className="carousel-item">
+    //         <img src="src\assets\lib\terror-background.jpg" className="d-block w-100" alt="..." />
+    //       </div>
+    //     </div>
+    //     <button 
+    //       className="carousel-control-prev" 
+    //       type="button" 
+    //       data-bs-target="#carouselExampleIndicators" 
+    //       data-bs-slide="prev"
+    //     >
+    //       <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    //       <span className="visually-hidden">Previous</span>
+    //     </button>
+    //     <button 
+    //       className="carousel-control-next" 
+    //       type="button" 
+    //       data-bs-target="#carouselExampleIndicators" 
+    //       data-bs-slide="next"
+    //     >
+    //       <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    //       <span className="visually-hidden">Next</span>
+    //     </button>
+    //   </div>
+    // </div>
+    
+    <div> <AddMusic />
+    {/* <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ' /> */}
     </div>
   );
 }
--- src/components/AddMusic.jsx
+++ src/components/AddMusic.jsx
@@ -1,9 +1,127 @@
-import React from 'react'
+import ReactPlayer from "react-player/youtube";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function AddMusic() {
+  // set state de las canciones para guardar en la memoria lo capturado por el formulario
+  const [newArtist, setNewArtist] = useState("");
+  const [newSong, setNewSong] = useState("");
+  const [newYouTube, setNewYouTube] = useState("");
+  const [newComment, setNewComment] = useState("");
+  
+  // esto es pa probar.
+  const [videoTest, setVideoTest] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+     const newVideo = {
+      newArtist,
+      newSong,
+      newYouTube,
+      newComment,
+    };
+    try {
+      const response = await axios.post(
+        "https://cats-dogs-abner.adaptable.app/music",
+        newVideo
+      );
+      setVideoTest(newVideo);
+    } catch (error) {
+      console.log("Error posting new video, ====>", error);
+    }
+  };
+
+  const getMusic = () => {
+    axios
+      .get("https://cats-dogs-abner.adaptable.app/music")
+      .then((response) => {
+        console.log("Music API fetch request ====>", response.data);
+             })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getMusic();
+  }, [videoTest]);
+
+  const handleArtistChange = (e) => {
+    setNewArtist(e.target.value);
+    console.log("This is the changing artist name", newArtist);
+  };
+
+  const handleTitleChange = (e) => {
+    setNewSong(e.target.value);
+    console.log("this is the changing song name", newSong);
+  };
+
+  const handleYouTubeChange = (e) => {
+    setNewYouTube(e.target.value);
+    console.log("this is the changing youtube url", newYouTube);
+  };
+
+  const handleNewCommentChange = (e) => {
+    setNewComment(e.target.value);
+    console.log("this is changing the comments", newComment);
+  };
+
   return (
-    <div>AddMusic</div>
-  )
+    <div className="add-music-container">
+      <form
+        onSubmit={handleSubmit}
+        className="m-50 d-flex flex-wrap align-items-left gap-5"
+      >
+        {/* <div> className="mb-50 align-center" */}
+        {/* </div> */}
+        {/* <div className="mb-50"> */}
+        <label for="artistName" className="form-label"></label>
+        <input
+          type="text"
+          placeholder="Artist"
+          className="form-control"
+          name="artistName"
+          onChange={handleArtistChange}
+        />
+        {/* </div> */}
+        {/* <div className="mb-50"> */}
+        <label for="songTitle" className="form-label"></label>
+        <input
+          type="text"
+          placeholder="Song Title"
+          className="form-control"
+          name="songTitle"
+          onChange={handleTitleChange}
+        />
+        {/* </div> */}
+        {/* <div className="m-50"> */}
+        <label for="youTubeUrl" className="form-label"></label>
+        <input
+          type="url"
+          placeholder="Youtube Link"
+          className="form-control"
+          name="youTubeUrl"
+          onChange={handleYouTubeChange}
+        />
+
+        {/* </div> */}
+        {/* <div className="m-50"> */}
+        <label for="commentBox" className="form-label"></label>
+        <input
+          type="text"
+          placeholder="leave a comment"
+          className="form-control"
+          name="commentBox"
+          onChange={handleNewCommentChange}
+        />
+        {/* </div> */}
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default AddMusic
+export default AddMusic;
