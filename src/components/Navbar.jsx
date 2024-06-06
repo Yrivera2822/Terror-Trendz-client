e17a89a62ad973c71be345db7f9874115c8b0fd4
Abner Nieves <nieves.abner@gmail.com>
2024-06-06 10:08:03 -0400

    new npm install

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link active text-primary ps-5" aria-current="page" to="/">Homepage</Link>
+                <Link className="nav-link active text-primary ps-5 icon-link icon-link-hover" href="#" aria-current="page" to="/">Homepage</Link>
               </li>
               <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle text-primary ps-5" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
