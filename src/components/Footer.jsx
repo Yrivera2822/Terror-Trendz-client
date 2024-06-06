1333f28abd7f01e27094c7b3643d3bb5be7bda3c
Abner Nieves <nieves.abner@gmail.com>
2024-06-06 17:19:04 -0400

    footer adjustment laptop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,54 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="bg-warning">
-        {/* <div className='footer-top'> 
+    <footer className="bg-warning float-sm-end">
+      {/* <div className='footer-top'> 
           <p>JOIN THE TERROR CLUB NOW</p> 
         </div> */}
 
-       <img src="https://res.cloudinary.com/dlbhhobaw/image/upload/v1717685535/arcade-fast-game-over_w8nqdr.gif" className="img-fluid"  alt="F-img" />
-   
-     
-        <div className="footer-content" style={{height: "70px", color:"#4b0082"}} >
-          <h3>Contact us:</h3>
-          <p>Email: Terrorfriends@gmail.com</p>
-          <p>Phone: 723-780-3903</p>
-          <p>1601 Apt #201 Ave Fernandez Juncos San Juan PR.</p>
-        </div>
+      <img
+        src="https://res.cloudinary.com/dlbhhobaw/image/upload/v1717685535/arcade-fast-game-over_w8nqdr.gif"
+        className="img-fluid"
+        alt="F-img"
+      />
 
-        
-        <div className="footer-content">
-           
-    
-          <h3 className='follow-us' style={{color:"#4b0082"}}>Follow Us:</h3>
-          <ul className="social-media d-flex">
-             <Link to= "https://www.youtube.com/watch?v=xvFZjo5PgG0" target="blank">  
-            <img src="https://res.cloudinary.com/dlbhhobaw/image/upload/v1717685535/logo-black_nvbkev.png" className="T-logo"  alt="T-img" />
-            </Link>
-            
-            <Link to="https://www.instagram.com/terror.trendz/" target="_blank" >
-            <img src="https://res.cloudinary.com/dlbhhobaw/image/upload/v1717685535/Instagram_Glyph_Black_rjzxcn.png" className="T-logo"  alt="T-img" />
-            </Link>
+      <div
+        className="footer-content"
+        style={{ height: "70px", color: "#4b0082" }}
+      >
+        <h3>Contact us:</h3>
+        <p>Email: Terrorfriends@gmail.com</p>
+        <p>Phone: 723-780-3903</p>
+        <p>1601 Apt #201 Ave Fernandez Juncos San Juan PR.</p>
+      </div>
 
-            <Link to="https://www.facebook.com/people/Terror-Trendz/100088390365188/" target="blank" alt= "T-img">
-            <img src="https://res.cloudinary.com/dlbhhobaw/image/upload/v1717702955/Facebook_Logo_Secondary_eib83f.png" className="T-logo"  alt="T-img" />
-            </Link>
-           
+      <div className="footer-content">
+        <h3 className="follow-us" style={{ color: "#4b0082" }}>
+          Follow Us:
+        </h3>
+        <ul className="social-media d-flex">
+          <Link to="https://www.youtube.com/watch?v=xvFZjo5PgG0" target="blank">
+            <img
+              src="https://res.cloudinary.com/dlbhhobaw/image/upload/v1717685535/logo-black_nvbkev.png"
+              className="T-logo"
+              alt="T-img"
+            />
+          </Link>
 
-          </ul>
-        </div>
-     
+          <Link to="https://www.instagram.com/terror.trendz/" target="_blank">
+            <img
+              src="https://res.cloudinary.com/dlbhhobaw/image/upload/v1717685535/Instagram_Glyph_Black_rjzxcn.png"
+              className="T-logo"
+              alt="T-img"
+            />
+          </Link>
+
+          <Link
+            to="https://www.facebook.com/people/Terror-Trendz/100088390365188/"
+            target="blank"
+            alt="T-img"
+          >
+            <img
+              src="https://res.cloudinary.com/dlbhhobaw/image/upload/v1717702955/Facebook_Logo_Secondary_eib83f.png"
+              className="T-logo"
+              alt="T-img"
+            />
+          </Link>
+        </ul>
+      </div>
     </footer>
   );
 }
 
 export default Footer;
 
-      
-    {/* <footer>
+{
+  /* <footer>
     
       <div className="footer-container" >
       <img src="src/assets/lib/arcade-fast-game-over.gif" className="img-fluid" alt="F-img" />
@@ -98,8 +115,5 @@ export default Footer;
       </div>
     </footer>
   );
-} */}
-
-
-
-
+} */
+}
